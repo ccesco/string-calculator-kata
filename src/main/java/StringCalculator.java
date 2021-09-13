313a17a0b505ffad44509fbf0aae0e9ca565ac5a
cyril <cyril.cesco@gmail.com>
2021-09-13 22:17:40 +0200

    add code for part 4

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,10 +4,34 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
-    public static final String SEPARATORS_REGEX = "[\n,]";
+    public static final String DEFAULT_SEPARATORS = "[\n,]";
+    public static final String BEGINNING_STRING = "//";
+    public static final String SEPARATOR_DELIMITER_NUMBER = "\n";
 
     public int add(String numbers) {
-        return Arrays.stream(numbers.split(SEPARATORS_REGEX)).
+        String[] delimiterNumber = numbers.split(SEPARATOR_DELIMITER_NUMBER);
+        String delimiter = getDelimiter(delimiterNumber[0]);
+        if (delimiter.equals(DEFAULT_SEPARATORS)) {
+            return addWithSeparator(
+                    numbers,
+                    delimiter
+            );
+        }
+        return addWithSeparator(
+                delimiterNumber[1],
+                delimiter
+        );
+    }
+
+    private String getDelimiter(String possibleDelimiter) {
+        if (possibleDelimiter.contains(BEGINNING_STRING)) {
+            return possibleDelimiter.replace(BEGINNING_STRING, "");
+        }
+        return DEFAULT_SEPARATORS;
+    }
+
+    private int addWithSeparator(String numbers, String separator) {
+        return Arrays.stream(numbers.split(separator)).
                 filter(StringUtils::isNoneEmpty)
                 .map(Integer::valueOf)
                 .reduce(Integer::sum)

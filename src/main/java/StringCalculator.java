b5616e9c1545c08f23fa16acd94af0f6b35a9456
cyril <cyril.cesco@gmail.com>
2021-09-13 21:36:23 +0200

    add code for part 3

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
-    public static final String SEPARATOR = ",";
+    public static final String SEPARATORS_REGEX = "[\n,]";
 
     public int add(String numbers) {
-        return Arrays.stream(numbers.split(SEPARATOR)).
+        return Arrays.stream(numbers.split(SEPARATORS_REGEX)).
                 filter(StringUtils::isNoneEmpty)
                 .map(Integer::valueOf)
                 .reduce(Integer::sum)

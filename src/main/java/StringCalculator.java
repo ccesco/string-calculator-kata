ac1cd7465ced2e3c073e64ab096c8970f923e1ab
cyril <cyril.cesco@gmail.com>
2021-09-13 21:00:36 +0200

    add code for part 1

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,16 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+
 public class StringCalculator {
 
+    public static final String SEPARATOR = ",";
+
     public int add(String numbers) {
-        return 100;
+        return Arrays.stream(numbers.split(SEPARATOR)).
+                filter(StringUtils::isNoneEmpty)
+                .map(Integer::valueOf)
+                .reduce(Integer::sum)
+                .orElse(0);
     }
 }
